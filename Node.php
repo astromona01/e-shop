a0112051b207e8235c46a851c1fb364a1295636b
Abdullah <www.sar.ras@gmail.com>
2022-02-06 12:03:33 -0500

    add tree to main page

--- Node.php
+++ Node.php
@@ -1,28 +1,51 @@
-<?php
-    include './NodeInterface.php'
+<?php 
 
-    class Node implements NodeInterface {
+class Node implements NodeInterface
+{
 
-        function addChild(child){
-            echo '+++' + child
-        }
-        function getChildren() {
+    private string $name = '';
 
-        }
-        function __construct() {
-
-        }
-        function __toString() {
-
-        }
-        function getName() {
-
-        }
+    private array $children = [];
+    
+    public function __construct(string $name)
+    {
+        $this->name = $name;
     }
 
-    /*
-        Возникла проблема реализации т.к. ранее не писал на php.
-        Прочитал в интернете подробнее про интерфейсы, думаю, что основную мысль понял,
-        создал класс Node с необходимыми функциями, однако с реализацией функционала возникли проблемы
-        Очень хотелось бы, чтобы подробнее разобрали это задание
+    public function __toString(): string
+    {
+        return self::print($this);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Node[]
      */
+    public function getChildren(): array
+    {
+        return $this->children;
+    }
+
+    public function addChild(Node $node): self
+    {
+        $this->children[] = $node;
+
+        return $this;
+    }
+
+    private static function print(Node $node, $level = 1)
+    {
+        $html = str_repeat('+ ', $level) . $node->getName()."\n";
+
+        foreach ($node->getChildren() as $child) {
+            $html .= self::print($child, $level + 1);
+        }
+
+        return $html;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,22 +1,23 @@
+
 <?php
 
-// include './NodeInterface.php';
-// include './Node.php';
+include './NodeInterface.php';
+include './Node.php';
 
-// $tree = (new \Node('Electronics'))
-//     ->addChild(
-//         (new \Node('Televisions'))
-//             ->addChild(new \Node('Tube'))
-//             ->addChild(new \Node('LCD'))
-//             ->addChild(new \Node('Plasma'))
-//         )
-//     ->addChild(
-//         (new \Node('Portable electronic'))
-//             ->addChild((new \Node('MP3 players'))->addChild(new \Node('Flash')))
-//             ->addChild(new \Node('CD players'))
-//             ->addChild(new \Node('2 way radios'))
-//         )    
-//     ;
+$tree = (new \Node('Electronics'))
+    ->addChild(
+        (new \Node('Televisions'))
+            ->addChild(new \Node('Tube'))
+            ->addChild(new \Node('LCD'))
+            ->addChild(new \Node('Plasma'))
+        )
+    ->addChild(
+        (new \Node('Portable electronic'))
+            ->addChild((new \Node('MP3 players'))->addChild(new \Node('Flash')))
+            ->addChild(new \Node('CD players'))
+            ->addChild(new \Node('2 way radios'))
+        )    
+    ;
 ?>
 <html>
     <body>
